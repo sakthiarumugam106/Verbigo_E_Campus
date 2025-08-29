283a91c246725057710d431796d54c534cb7f330
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-08-29 17:53:54 +0000

    not matching

--- src/components/verbigo-logo.tsx
+++ src/components/verbigo-logo.tsx
@@ -9,10 +9,7 @@ export function VerbigoLogo({ className, ...props }: React.SVGProps<SVGSVGElemen
       className={cn("fill-current", className)}
       {...props}
     >
-      <path d="M50,95 C25,95 25,65 50,65 C75,65 75,95 50,95 Z" />
-      <path d="M10,60 L50,40 L90,60 L50,80 Z" />
-      <path d="M10,45 L50,25 L90,45 L50,65 Z" />
-      <path d="M10,30 L50,10 L90,30 L50,50 Z" />
+      <path d="M50 10 L10 30 L10 70 L50 90 L90 70 L90 30 Z M 50 20 L80 35 L50 50 L20 35 Z" />
     </svg>
   );
 }
