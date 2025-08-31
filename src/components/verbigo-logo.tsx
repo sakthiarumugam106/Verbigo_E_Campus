f60b66c48a2e5ceb218995ae5c34a39f0226c6c0
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-08-31 08:19:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/verbigo-logo.tsx
+++ src/components/verbigo-logo.tsx
@@ -1,12 +1,11 @@
 
-import { cn } from "@/lib/utils";
-
-export function VerbigoLogo({ className }: { className?: string }) {
+export function VerbigoLogo() {
   return (
     <img
       src="/logo.png"
       alt="Verbigo Logo"
-      className={cn("h-10 w-auto", className)}
+      width="40"
+      height="40"
     />
   );
 }
