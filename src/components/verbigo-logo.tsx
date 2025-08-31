9ac04608b30933ed1f15d4dc6f24cb1c46f780db
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-08-31 07:32:11 +0000

    no, dont design logo yoursef, just replace image , what ever i given

--- src/components/verbigo-logo.tsx
+++ src/components/verbigo-logo.tsx
@@ -1,24 +1,16 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import * as React from "react";
 
-export function VerbigoLogo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
+export function VerbigoLogo({ className, ...props }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("fill-primary", className)}
-      {...props}
-    >
-      <path d="M22 10l-10-5-10 5 10 5 10-5z" />
-      <path d="M6 12v5l6 3 6-3v-5" />
-      <path d="M6 12l6 3 6-3" />
-      <path d="M6 12L2 10l10-5 10 5-4 2" />
-      <path d="M2 10v5l6 3 2-1" />
-    </svg>
+    <Image
+      src="/images/logo.png"
+      alt="Verbigo Logo"
+      width={40}
+      height={40}
+      className={cn(className)}
+      priority // Ensures the logo loads quickly
+    />
   );
 }
