e28b15ae786b3c8f0b2c3c7e42d73345d26eee52
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-08-31 08:18:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -220,7 +220,7 @@ export default function HomePage() {
               </div>
             </div>
             <Image
-              src="https://picsum.photos/1200/800"
+              src="https://images.unsplash.com/photo-1462536943532-57a629f6cc60?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxlZHVjYXRpb258ZW58MHx8fHwxNzU2NjI4MjMwfDA&ixlib=rb-4.1.0&q=80&w=1080"
               alt="Hero"
               width={1200}
               height={800}
