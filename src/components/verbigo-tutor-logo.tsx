518dadb422698914c22fed9c214d654a136fdfc2
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 13:42:24 +0000

    still icon png not loading

--- src/components/verbigo-tutor-logo.tsx
+++ src/components/verbigo-tutor-logo.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import Image from 'next/image';
-
 export function VerbigoTutorLogo({ width = 48, height = 48 }: { width?: number; height?: number; }) {
   return (
-    <Image
+    <img
       src="/chatbot.png"
       alt="Verbigo Tutor"
       width={width}
       height={height}
-      className="object-contain"
+      style={{ objectFit: 'contain' }}
     />
   );
 }
