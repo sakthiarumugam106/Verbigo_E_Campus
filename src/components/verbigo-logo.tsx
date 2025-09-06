267135f35db823d3a75319b1cef6d40e419fc1f7
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 12:30:08 +0000

    why image not loading? (_for element img_)

--- src/components/verbigo-logo.tsx
+++ src/components/verbigo-logo.tsx
@@ -1,11 +1,10 @@
 
 import Image from 'next/image';
-import logo from '@/logo.png';
 
 export function VerbigoLogo() {
   return (
     <Image
-      src={logo}
+      src="/logo.png"
       alt="Verbigo Logo"
       width="40"
       height="40"
