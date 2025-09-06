a8de986b11a3c017f965d80497dcad3db02e9359
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 13:22:49 +0000

    use this image

--- src/components/verbigo-tutor-logo.tsx
+++ src/components/verbigo-tutor-logo.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export function VerbigoTutorLogo({ width = 48, height = 48 }: { width?: number; height?: number; }) {
   return (
     <Image
-      src="https://img.icons8.com/flat-round/64/bot.png"
+      src="https://firebasestudio-hosting.web.app/verbigo/chatbot-icon.png"
       alt="Verbigo Tutor"
       width={width}
       height={height}
