0591f2bd2c0a7c9a92699d775b6e4f2b6ed909ba
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 08:02:38 +0000

    Our Courses (_for element h2_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ export default function HomePage() {
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Our Language & Writing Courses
+                Our Courses
               </h2>
               <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Whether you're starting from scratch or honing your expertise, we have a course for you.
