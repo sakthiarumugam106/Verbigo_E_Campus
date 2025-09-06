45303dffcb183baf04ad8c98d81173b2ea3a9bda
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 12:31:54 +0000

    revert ur recent chanes, now main logo got affected

--- src/components/verbigo-logo.tsx
+++ src/components/verbigo-logo.tsx
@@ -1,10 +1,11 @@
 
 import Image from 'next/image';
+import logo from '../logo.png';
 
 export function VerbigoLogo() {
   return (
     <Image
-      src="/logo.png"
+      src={logo}
       alt="Verbigo Logo"
       width="40"
       height="40"
