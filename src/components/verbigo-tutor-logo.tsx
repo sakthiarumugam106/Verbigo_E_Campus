888cf42550b01983df70f9be9ff4181e6c5021c3
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 12:43:10 +0000

    use this exact icon
    <img width="100" height="100" src="https://img.icon

--- src/components/verbigo-tutor-logo.tsx
+++ src/components/verbigo-tutor-logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function VerbigoTutorLogo({ width = 48, height = 48 }: { width?: number; height?: number; }) {
   return (
     <Image
-      src="https://img.icons8.com/plasticine/100/bot.png"
+      src="https://img.icons8.com/external-flat-juicy-fish/100/external-ai-contact-us-flat-flat-juicy-fish-2.png"
       alt="Verbigo Tutor"
       width={width}
       height={height}
