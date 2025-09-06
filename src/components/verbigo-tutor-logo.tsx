6ddbbb308c73115d03ac9c68ae7ebe89f77c6ed3
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 13:41:14 +0000

    use this icon public/chatbot.png

--- src/components/verbigo-tutor-logo.tsx
+++ src/components/verbigo-tutor-logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export function VerbigoTutorLogo({ width = 48, height = 48 }: { width?: number; height?: number; }) {
   return (
     <Image
-      src="https://img.icons8.com/plasticine/100/bot.png"
+      src="/chatbot.png"
       alt="Verbigo Tutor"
       width={width}
       height={height}
