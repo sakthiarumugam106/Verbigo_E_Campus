9937b114e95f2c39199fb5ec2530f0e1b173e9f1
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 12:30:57 +0000

    now both logo got affected, dont touch main logo\

---
+++ src/components/verbigtutor-logo.tsx
@@ -0,0 +1,14 @@
+
+import Image from 'next/image';
+import logo from '../logo.png';
+
+export function VerbigoLogo() {
+  return (
+    <Image
+      src={logo}
+      alt="Verbigo Logo"
+      width="40"
+      height="40"
+    />
+  );
+}
