2192e315656f96344e8e94242b59172b753bc80b
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-06 13:21:11 +0000

    <div> Icons made by <a href="https://www.flaticon.com/authors/juicy-fish

--- src/components/verbigo-tutor-logo.tsx
+++ src/components/verbigo-tutor-logo.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export function VerbigoTutorLogo({ width = 48, height = 48 }: { width?: number; height?: number; }) {
   return (
     <Image
-      src="https://img.icons8.com/external-flat-juicy-fish/100/external-ai-contact-us-flat-flat-juicy-fish-2.png"
+      src="https://img.icons8.com/flat-round/64/bot.png"
       alt="Verbigo Tutor"
       width={width}
       height={height}
