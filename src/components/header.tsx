2a6a32ac7ff3e892222b162607eff17ca9747d3c
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-08 08:42:13 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,8 +24,8 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between px-4 md:px-6">
-        <div className="flex items-center gap-6 md:gap-10">
+      <div className="container flex h-16 items-center px-4 md:px-6">
+        <div className="mr-auto flex items-center gap-6 md:gap-10">
             <Link href="/" className="flex items-center gap-2 text-primary dark:text-primary-foreground">
                 <VerbigoLogo />
                 <div className="flex flex-col">
