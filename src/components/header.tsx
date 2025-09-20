22c465ad06f4414fa754614058df9ad967cb3465
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-20 12:59:07 +0000

    make center for all options in header (_for element <Header>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,8 +25,8 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center px-4 md:px-6">
-        <div className="mr-auto flex items-center gap-6 md:gap-10">
+      <div className="container flex h-16 items-center justify-between px-4 md:px-6">
+        <div className="flex items-center">
             <Link href="/" className="flex items-center gap-2 text-primary dark:text-primary-foreground">
                 <VerbigoLogo />
                 <div className="flex flex-col">
@@ -34,18 +34,19 @@ export function Header() {
                     <span className="text-[10px] font-medium text-muted-foreground leading-none mt-1">E-Campus for Language Intelligence</span>
                 </div>
             </Link>
-            <nav className="hidden items-center justify-center gap-6 text-sm font-medium md:flex">
-                {navLinks.map((link) => (
-                    <Link
-                    key={link.name}
-                    href={link.href}
-                    className="transition-colors hover:text-foreground/80 text-foreground/60"
-                    >
-                    {link.name}
-                    </Link>
-                ))}
-            </nav>
         </div>
+        
+        <nav className="hidden items-center justify-center gap-6 text-sm font-medium md:flex">
+            {navLinks.map((link) => (
+                <Link
+                key={link.name}
+                href={link.href}
+                className="transition-colors hover:text-foreground/80 text-foreground/60"
+                >
+                {link.name}
+                </Link>
+            ))}
+        </nav>
         
         <div className="flex items-center gap-2">
             <div className="hidden sm:flex items-center gap-2">
