3b10a1fa10f57d47cde98264165ca2e41e60f25a
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-23 18:35:34 +0000

    defalt image fill color should green (_for element img_)

--- src/components/whatsapp-button-icon.tsx
+++ src/components/whatsapp-button-icon.tsx
@@ -6,7 +6,7 @@ export const WhatsAppButtonIcon = ({ className }: { className?: string }) => (
     <Image
         width="150"
         height="150"
-        src="https://img.icons8.com/ios-filled/150/FFFFFF/whatsapp--v1.png"
+        src="https://img.icons8.com/ios-filled/150/008000/whatsapp--v1.png"
         alt="whatsapp"
         className={cn("h-full w-full", className)}
     />
