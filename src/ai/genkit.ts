2b50a94d27d0c9a478126d0a8ffeb2f9ba2e5331
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-27 07:43:25 +0000

    why its getting failed

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: 'v1'})],
   model: 'googleai/gemini-1.5-flash',
 });
