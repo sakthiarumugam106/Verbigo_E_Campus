cc5673ee2de6f606acedc3dc3fae7f1fb943e951
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-27 08:28:12 +0000

    add needed depedency for 2.0 model, it should be work

--- src/ai/flows/text-to-speech-flow.ts
+++ src/ai/flows/text-to-speech-flow.ts
@@ -63,7 +63,7 @@ const textToSpeechFlow = ai.defineFlow(
   },
   async (query) => {
     const { media } = await ai.generate({
-      model: googleAI.model('gemini-2.5-flash-preview-tts'),
+      model: googleAI.model('gemini-1.5-flash-preview-tts'),
       config: {
         responseModalities: ['AUDIO'],
         speechConfig: {
