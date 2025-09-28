d939e0f75e6fbfe894b08c92804ead51bf5d41b9
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-28 14:01:37 +0000

    whithout hover icon color should white (_for element img_)

--- src/components/whatsapp-button-icon.tsx
+++ src/components/whatsapp-button-icon.tsx
@@ -7,14 +7,14 @@ export const WhatsAppButtonIcon = ({ className }: { className?: string }) => (
     <Image
         width="150"
         height="150"
-        src="https://img.icons8.com/ios-filled/150/008000/whatsapp--v1.png"
+        src="https://img.icons8.com/ios-filled/150/FFFFFF/whatsapp--v1.png"
         alt="whatsapp"
         className="absolute inset-0 h-full w-full transition-opacity group-hover:opacity-0"
     />
     <Image
         width="150"
         height="150"
-        src="https://img.icons8.com/ios-filled/150/FFFFFF/whatsapp--v1.png"
+        src="https://img.icons8.com/ios-filled/150/008000/whatsapp--v1.png"
         alt="whatsapp"
         className="absolute inset-0 h-full w-full opacity-0 transition-opacity group-hover:opacity-100"
     />
