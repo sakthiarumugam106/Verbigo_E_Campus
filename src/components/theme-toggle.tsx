2646af44de113697e7c5eaea0813c48fb7a2624a
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-09-28 05:08:36 +0000

    i ask you to change only theme toggle button

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -20,8 +20,7 @@ export function ThemeToggle() {
       variant="outline" 
       size="icon" 
       onClick={toggleTheme}
-      className="neumorphic-inner"
-      useNeumorphic={false}
+      className="theme-toggle-button"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
