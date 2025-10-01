4da935031bd85da4cbd933104d0ec8fbb4bea667
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-10-01 04:03:26 +0000

    move all header to text-left only on mobile view and our core value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,7 @@ function ValuesSection() {
                         <AccordionTrigger className="p-4 hover:no-underline w-full">
                             <div className="flex items-center justify-start gap-4">
                                 {value.icon}
-                                <h3 className="text-xl font-semibold text-primary dark:text-primary-foreground">{value.title}</h3>
+                                <h3 className="text-xl font-semibold text-left text-primary dark:text-primary-foreground">{value.title}</h3>
                             </div>
                         </AccordionTrigger>
                         <AccordionContent className="p-4 pt-0 text-left">
