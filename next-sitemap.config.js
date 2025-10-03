6488f9ac5ee2ed73d52279ea105888402cd8fb04
Sakthivel Arumugam <sakthiarumugam106@gmail.com>
2025-10-03 09:10:11 +0000

    is this tag added for home page
    <link rel="canonical" href="https://ver

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,21 @@ module.exports = {
   sitemapSize: 7000,
   trailingSlash: true,
   additionalPaths: async (config) => [
-    { loc: '/' }
+    { loc: '/', changefreq: 'daily', priority: 1.0, lastmod: new Date().toISOString() }
   ],
+   transform: async (config, path) => {
+    // Return null to exclude a path
+    if (path.match(/api/)) {
+      return null;
+    }
+
+    // Use default transformation for all other paths
+    return {
+      loc: path, 
+      changefreq: config.changefreq,
+      priority: path === '/' ? 1.0 : config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  }
 };
